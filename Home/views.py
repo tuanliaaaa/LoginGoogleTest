cefbadba5670e9a01f9c04d137e374259a9e648d
tuanliaaaa <nhattuan44t@gmail.com>
2023-03-28 21:13:09 +0700

    changeStatus

--- Home/views.py
+++ Home/views.py
@@ -23,8 +23,8 @@ class LoginByGoogle(View):
         authorization_url, state = flow.authorization_url(
             
             access_type='offline',
-        
-            #  include_granted_scopes='true'
+            state='home',
+             include_granted_scopes='true'
              )
         session = SessionStore()
         request.session['state'] = state
@@ -34,13 +34,16 @@ class LoginByGoogle(View):
 class LoginGoogleResponse(View):
     def get(self,request):
         state = request.session.get('state','')
+        
         flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
             'Home/client_secret.json',
-            scopes=['https://www.googleapis.com/auth/userinfo.profile'],
-            state=state)
+            scopes=['https://www.googleapis.com/auth/userinfo.profile']
+            ,state=state
+            )
         flow.redirect_uri = request.build_absolute_uri(reverse('oauth2callback'))
+        
         authorization_response = request.build_absolute_uri()
-        print(authorization_response)
+        print("đay la url response: ",authorization_response)
         flow.fetch_token(authorization_response=authorization_response)
 
         credentials = flow.credentials
@@ -52,21 +55,25 @@ class LoginGoogleResponse(View):
             'client_id': credentials.client_id,
             'client_secret': credentials.client_secret,
             'scopes': credentials.scopes}
+        print(credentials.refresh_token)
         session.save()
-        return request.build_absolute_uri(reverse('home'))
+        return redirect(request.build_absolute_uri(reverse('home')))
 
 class Home(View):
     def get(self,request):
         credentials_dict = request.session.get('credentials')
+        print(credentials_dict)
         if not credentials_dict:
             return redirect('authorize')
 
         credentials = Credentials.from_authorized_user_info(info=credentials_dict)
-
+        
         
         try:
             service = build('people', 'v1', credentials=credentials)
-            person = service.people().get(resourceName='people/me', personFields='names,emailAddresses').execute()
+            print(service)
+            person = service.people().get(resourceName='people/me', personFields='names').execute()
+            print('day la',person)
             return render(request, 'person.html', {'person': person})
         except HttpError as error:
             return render(request, 'error.html', {'error': error})
